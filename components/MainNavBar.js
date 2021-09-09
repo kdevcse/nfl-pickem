071a388bb64e7c1e73202018bdd6f118aab89527
klewiscse <klewiscse@gmail.com>
2021-09-09 01:33:46 -0500

    Add eslint rule for indention

--- components/MainNavBar.js
+++ components/MainNavBar.js
@@ -5,13 +5,13 @@ import styles from '../styles/MainNavBar.module.css';
 
 export default function MainNavBar() {
   return (
-  <NavBar bg='primary' variant='dark'>
-    <Container>
-      <Nav activeKey='#home'>
-        <NavBar.Brand className={styles.navBrand} href='#home'>PickEm</NavBar.Brand>
-        <Nav.Link href='#home'>Home</Nav.Link>
-        <Nav.Link href='#history'>History</Nav.Link>
-      </Nav>
-    </Container>
-  </NavBar>);
+    <NavBar bg='primary' variant='dark'>
+      <Container>
+        <Nav activeKey='#home'>
+          <NavBar.Brand className={styles.navBrand} href='#home'>PickEm</NavBar.Brand>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#history'>History</Nav.Link>
+        </Nav>
+      </Container>
+    </NavBar>);
 }
