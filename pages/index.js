f2045dc694a7567169bba2e19de783acfcf03de9
Kevin <klewiscse@gmail.com>
2021-11-10 15:57:56 -0600

    Implement firestore
    Implement firestore

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import styles from '../styles/Home.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { firestore } from '../utilities/firebaseConfig';
 
 
 export default function Home() {
@@ -11,19 +13,25 @@ export default function Home() {
     getData();
   });
 
-  async function getData(){
+  async function getData() {
+    const docRef = doc(firestore, 'schedule', 'hi2cZUU82QGq7YRlR7VR');
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+    } else {
+      console.log('data not found');
+    }
   }
 
   return (
     <>
-      {
-        <div className={styles.homeContainer}>
-          <MainNavBar></MainNavBar>
-          <Container>
-            <h2 className={styles.welcomeTitle}>Welcome to the NFL Pick-em App</h2>
-          </Container>
-        </div>
-      }
+      <div className={styles.homeContainer}>
+        <MainNavBar></MainNavBar>
+        <Container>
+          <h2 className={styles.welcomeTitle}>Welcome to the NFL Pick-em App</h2>
+        </Container>
+      </div>
     </>
   )
 }
